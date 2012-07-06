64e1fc4caffcdf696ab91c842a1ad92bd2699a40
Nicholas Szandor Hakobian <nhakobian@gmail.com>
2012-07-06 16:27:26 -0500

    initial

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+require 'inc/head_foot.inc';
+
+$title = "Test";
+phead();
+?>
+
+<div class="right" style="width:425px;height:359px;">
+  <img src='static/tadpol.jpg'></img>
+</div>
+
+<h2>General Information</h2>
+
+<p>
+  The TADPOL survey uses the 1mm dual-polarization system at CARMA to
+  probe the morphology of magnetic fields in dense, star-forming
+  cores.  Dust polarization maps with ~2'' angular resolution reveal
+  whether the fields on scales of 0.005 pc are aligned with
+  large-scale fields in the associated molecular-cloud envelopes, or
+  with the orientations of bipolar outflows. The TADPOL collaboration
+  includes members from all CARMA institutions (Berkeley, Caltech,
+  Illinois, Maryland, and Chicago), as well as the University of
+  Manitoba, the University of Western Ontario, the University of
+  Arizona, the Herzberg Institute of Astrophysics, and the SOFIA
+  Science Center.
+</p>
+
+<div class="clear"></div>
+
+<?php pfoot();?>
