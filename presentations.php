4b6078c50da7f851611d5891218dd6f6a2d88115
Nicholas Szandor Hakobian <nhakobian@gmail.com>
2012-07-19 15:00:56 -0500

    Added presentations

---
+++ presentations.php
@@ -0,0 +1,90 @@
+<?php
+require 'inc/head_foot.inc';
+
+$title = "Presentations";
+phead();
+?>
+
+<h2 style="text-align:center">Presentations</h2>
+
+<ul class="pres">
+  <li>
+    XXVIII IAU General Assembly, Special Session 4, 
+    <i>"New era for studying interstellar and intergalactic magnetic 
+         fields,"</i> 
+    Beijing, China (August 20-23, 2012)
+    <ul>
+      <li>
+        <b>Ian Stephens</b>,<i>"The magnetic field morphology of the Class 0 
+                                 protostar L1157"</i> (poster)
+      </li>
+    </ul>
+  </li>
+  <li>
+    MPIA Conference, <i>"The Early Phase of Star Formation,"</i>
+    Ringberg Castle, Germany (July 1-6, 2012)
+    <ul>
+      <li><b>Leslie Looney</b>, <i>"Changing the Paradigm for the Inner 
+          Envelope of Class 0 Protostars"</i> (talk)
+      </li>
+      <li><b>John Tobin</b>, <i>"Protostellar Envelopes"</i> (talk)</li>
+    </ul>
+  </li>
+  <li>
+    SOFIA Science Center colloquium, NASA Ames, Mountain View, CA
+    <ul>
+      <li>
+        <b>Chat Hull</b>, <i>"Polarization in Forming Stars: Probing Small 
+        Scales with CARMA"</i> (talk, May 23, 2012)
+      </li>
+      <li>
+        <b>Leslie Looney</b>, <i>"The Structure of Class 0 Protostars: 
+        Morphology and Evolution"</i> (talk, May 16, 2012)
+      </li>
+    </ul>
+  </li>
+  <li>
+    <i>"Revealing Evolution of Protoplanetary Disks in the ALMA Era,"</i>
+    Kyoto, Japan (May 8-11, 2012)
+    <ul>
+      <li>
+        <b>Meredith Hughes</b>, <i>"Dynamics of protoplanetary and debris 
+        disks: turbulence, magnetic fields, and clumps"</i> (talk)
+      </li>
+    </ul>
+  </li>
+  <li>
+    Visit to University of Manitoba, Winnipeg, MB, Canada (April 6, 2012)
+    <ul>
+      <li>
+        <b>Chat Hull</b>, Informal visit and discussion
+      </li>
+    </ul>
+  </li>
+  <li>
+    <i>"Midwest Magnetic Fields"</i> Workshop, Madison, WI (April 4, 2012)
+    <ul>
+      <li>
+        <b>Chat Hull</b>, <i>"Polarization in Forming Stars"</i> (talk)
+      </li>
+    </ul>
+  </li>
+  <li>
+    NRAO-NAASC Workshop <i>"Outflows, Winds, and Jets,"</i>
+    Charlottesville, VA (March 3-6, 2012)
+    <ul>
+      <li>
+        <b>Chat Hull</b>, <i>"Are outflows aligned with magnetic fields in 
+        forming stars?</i>" (poster)
+      </li>
+      <li>
+        <b>Leslie Looney</b>, <i>"Results from SMA, CARMA, Herschel, and ALMA 
+        Cycle 0 Programs"</i> (talk) 
+      </li>
+    </ul>
+  </li>
+</ul>
+
+<div class="clear"></div>
+
+<?php pfoot();?>
