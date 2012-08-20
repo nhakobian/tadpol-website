7e96fbbd5f053a60aa40eb30375f7d8340da9c87
Nicholas Szandor Hakobian <nhakobian@gmail.com>
2012-08-20 14:02:52 -0500

    Added website links.

--- people.php
+++ people.php
@@ -5,33 +5,33 @@ $title = "People";
 phead();
 
 $people = array(
-            array('Chat Hull', 'UC Berkeley', 'chat@astro.berkeley.edu'),
-            array('Dick Plambeck', 'UC Berkeley', 'plambeck@astro.berkeley.edu'),
-	    array('Alberto Bolatto', 'University of Maryland', 'bolatto@astro.umd.edu'),
-	    array('Geoff Bower', 'UC Berkeley', 'gbower@astro.berkeley.edu'),
-	    array('John Carpenter', 'Caltech', 'jmc@astro.caltech.edu'),
-	    array('Dick Crutcher', 'University of Illinois', 'crutcher@illinois.edu'),
-	    array('Jason Fiege', 'University of Manitoba', 'fiege@physics.umanitoba.ca'),
-	    array('Erica Franzmann', 'University of Manitoba', 'umfranze@cc.umanitoba.ca'),
-	    array('Nicholas Hakobian', 'University of Illinois', 'nhakobi2@illinois.edu'),
-	    array('Carl Heiles', 'UC Berkeley', 'heiles@astro.berkeley.edu'),
-	    array('Roger Hildebrand', 'University of Chicago', 'roger@oddjob.uchicago.edu'),
-	    array('Martin Houde', 'University of Western Ontario', 'mhoude2@uwo.ca'),
-	    array('Meredith Hughes', 'UC Berkeley', 'mhughes@astro.berkeley.edu'),
-	    array('Katherine Jameson', 'University of Maryland', 'kjameson@astro.umd.edu'),
-	    array('Woojin Kwon', 'University of Illinois', 'wkwon@illinois.edu'),
-	    array('James Lamb', 'Caltech', 'lamb@ovro.caltech.edu'),
-	    array('Leslie Looney', 'University of Illinois', 'lwl@illinois.edu'),
-	    array('Dan Marrone', 'University of Arizona', 'dmarrone@email.arizona.edu'),
-	    array('Brenda Matthews', 'National Research Council of Canada', 
-		  'Brenda.Matthews@nrc-cnrc.gc.ca'),
-	    array('Lee Mundy', 'University of Maryland', 'lgm@astro.umd.edu'),
-	    array('Thushara Pillai', 'Caltech', 'tpillai@astro.caltech.edu'),
-	    array('Marc Pound', 'University of Maryland',  'mpound@astro.umd.edu'),
-	    array('Ian Stephens', 'University of Illinois', 'stephen6@illinois.edu'),
-	    array('John Vaillancourt', 'SOFIA Science Center', 'jvaillancourt@sofia.usra.edu'),
-	    array('Nikolaus Volgenau', 'Caltech', 'volgenau@ovro.caltech.edu'),
-	    array('Melvyn Wright', 'UC Berkeley', 'wright@astro.berkeley.edu'),
+		array('Chat Hull', 'UC Berkeley', 'chat@astro.berkeley.edu', 'http://astro.berkeley.edu/~chat/'),
+		array('Dick Plambeck', 'UC Berkeley', 'plambeck@astro.berkeley.edu', 'http://astro.berkeley.edu/~plambeck/'),
+		array('Alberto Bolatto', 'University of Maryland', 'bolatto@astro.umd.edu', 'http://www.astro.umd.edu/people/bolatto.html'),
+		array('Geoff Bower', 'UC Berkeley', 'gbower@astro.berkeley.edu', 'http://astro.berkeley.edu/~gbower/'),
+		array('John Carpenter', 'Caltech', 'jmc@astro.caltech.edu', 'http://www.astro.caltech.edu/~jmc/'),
+		array('Dick Crutcher', 'University of Illinois', 'crutcher@illinois.edu', 'http://www.astro.uiuc.edu/people/crutcher'),
+		array('Jason Fiege', 'University of Manitoba', 'fiege@physics.umanitoba.ca', 'http://www.physics.umanitoba.ca/~fiege/'),
+		array('Erica Franzmann', 'University of Manitoba', 'umfranze@cc.umanitoba.ca', ''),
+		array('Nicholas Hakobian', 'University of Illinois', 'nhakobi2@illinois.edu', 'http://www.astro.uiuc.edu/people/nhakobi2'),
+		array('Carl Heiles', 'UC Berkeley', 'heiles@astro.berkeley.edu', 'http://astro.berkeley.edu/~heiles/'),
+		array('Roger Hildebrand', 'University of Chicago', 'roger@oddjob.uchicago.edu', 'http://astro.uchicago.edu/people/roger-h-hildebrand.shtml'),
+		array('Martin Houde', 'University of Western Ontario', 'mhoude2@uwo.ca', 'http://www.astro.uwo.ca/~houde/'),
+		array('Meredith Hughes', 'UC Berkeley', 'mhughes@astro.berkeley.edu', 'http://astro.berkeley.edu/~mhughes/Home.html'),
+		array('Katherine Jameson', 'University of Maryland', 'kjameson@astro.umd.edu', 'http://www.astro.umd.edu/people/kjameson.html'),
+		array('Woojin Kwon', 'University of Illinois', 'wkwon@illinois.edu', 'http://haneul.astro.illinois.edu/~wkwon/'),
+		array('James Lamb', 'Caltech', 'lamb@ovro.caltech.edu', 'http://www.ovro.caltech.edu/~lamb/'),
+		array('Leslie Looney', 'University of Illinois', 'lwl@illinois.edu', 'http://eeyore.astro.illinois.edu/'),
+		array('Dan Marrone', 'University of Arizona', 'dmarrone@email.arizona.edu', 'https://www.as.arizona.edu/people/faculty/marrone.html'),
+		array('Brenda Matthews', 'National Research Council of Canada', 
+		      'Brenda.Matthews@nrc-cnrc.gc.ca', 'http://astrowww.phys.uvic.ca/~brenda/'),
+		array('Lee Mundy', 'University of Maryland', 'lgm@astro.umd.edu', 'http://www.astro.umd.edu/people/lgm.html'),
+		array('Thushara Pillai', 'Caltech', 'tpillai@astro.caltech.edu', ''),
+		array('Marc Pound', 'University of Maryland',  'mpound@astro.umd.edu', 'http://www.astro.umd.edu/people/mpound.html'),
+		array('Ian Stephens', 'University of Illinois', 'stephen6@illinois.edu', 'http://www.astro.uiuc.edu/people/stephen6'),
+		array('John Vaillancourt', 'SOFIA Science Center', 'jvaillancourt@sofia.usra.edu', 'http://www.sofia.usra.edu/Science/team/jvaillancourt.html'),
+		array('Nikolaus Volgenau', 'Caltech', 'volgenau@ovro.caltech.edu', ''),
+		array('Melvyn Wright', 'UC Berkeley', 'wright@astro.berkeley.edu', 'http://astro.berkeley.edu/~wright/'),
 		);
 ?>
 
@@ -45,7 +45,12 @@ foreach ($people as $person)
 {
   $email = explode('@', $person[2]);
   echo '<tr class="trow">';
-  echo '<td class="name">'.$person[0].'</td>';
+  echo '<td class="name">';
+  if ($person[3] != '') {
+    echo '<a href="'.$person[3].'">'.$person[0].'</a></td>';
+  } else {
+    echo $person[0].'</td>';
+  }
   echo '<td class="inst">'.$person[1].'</td>';
   echo '<td class="email1">'.$email[0].'</td>';
   echo '<td class="emailat">_at_</td>';
