cfe9dcf42d7aea29953f252a4368c5dad8501a65
Chat Hull <chat.hull@gmail.com>
2013-03-04 22:16:52 -0800

    Updated.

--- papers.php
+++ papers.php
@@ -10,26 +10,26 @@ phead();
 <ul>
   <li>
     Hull, C. L. H., Plambeck, R. L., et al., <i> Misalignment of Magnetic 
-    Fields and Outflows in Protostellar Cores</i>, 2013, ApJL, (submitted)
+    Fields and Outflows in Protostellar Cores</i>, 2013, ApJ, (submitted)
   </li>
 
   <li>
     Hughes, A. M., Hull, C. L. H., Wilner, D. J., Plambeck, R. L.,
     <i>Interferometric Upper Limits on Millimeter Polarization of the Disks 
-    around DG Tau, GM Aur, and MWC 480</i>, 2013, AJ, (submitted)
+    around DG Tau, GM Aur, and MWC 480</i>, 2013, AJ, (accepted)
   </li>
 
   <li>
     Krumholz, M. R., Crutcher, R. M., Hull, C. L. H., <i>Protostellar Disk
     Formation Enabled by Weak, Misaligned Magnetic Fields</i>, 2013, ApJL,
-    (submitted)
+    (accepted)
   </li>
 
   <li>
-    Stephens, I., Looney, L., Hull, C., Plambeck, R., Kwon, W., Chapman, R., 
+    Stephens, I., Looney, L., Kwon, W., Hull, C. L. H., Plambeck, R., Chapman, N., 
     Novak, G., Matthews, T., Vaillancourt, J., Shinnaga, H., Davidson, J., and
     Crutcher, R., 
-    <i>The Magnetic Field Morphology of the Class 0 Protostar L1157</i>, 2012,
+    <i>The Magnetic Field Morphology of the Class 0 Protostar L1157</i>, 2013,
     (in prep)
   </li>
 </ul>
