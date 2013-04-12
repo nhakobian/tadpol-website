13db324166157c2ab26887e804790c94b8c7592b
Chat Hull <chat.hull@gmail.com>
2013-04-12 11:55:22 -1000

    Added ADS links.

--- papers.php
+++ papers.php
@@ -5,23 +5,26 @@ $title = "Papers";
 phead();
 ?>
 
-<h2 style="text-align:center;">Papers</h2>
+<h2 style="text-align:center;">Refereed Publications</h2>
 
 <ul class="papers">
   <li>
     Hull, C. L. H., Plambeck, R. L., et al., <i> Misalignment of Magnetic 
     Fields and Outflows in Protostellar Cores,</i> 2013, ApJ, accepted
+	[<a href="http://adsabs.harvard.edu/abs/2012arXiv1212.0540H">ADS</a>]
   </li>
 
   <li>
     Hughes, A. M., Hull, C. L. H., Wilner, D. J., Plambeck, R. L.,
     <i>Interferometric Upper Limits on Millimeter Polarization of the Disks 
     around DG Tau, GM Aur, and MWC 480,</i> 2013, AJ, 145, 115
+	[<a href="http://adsabs.harvard.edu/abs/2013AJ....145..115H">ADS</a>]
   </li>
 
   <li>
     Krumholz, M. R., Crutcher, R. M., Hull, C. L. H., <i>Protostellar Disk
     Formation Enabled by Weak, Misaligned Magnetic Fields,</i> 2013, ApJL, 767, L11
+	[<a href="http://adsabs.harvard.edu/abs/2013ApJ...767L..11K">ADS</a>]
   </li>
 
   <li>
